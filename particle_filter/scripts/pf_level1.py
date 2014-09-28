c6bc88b4dc6557414afa72e014288cebb4e4a10d
Eric Schneider <franz.eric.schneider@gmail.com>
2014-09-28 10:11:29 -0400

    Cleaned up some formatting, unimportant

--- particle_filter/scripts/pf_level1.py
+++ particle_filter/scripts/pf_level1.py
@@ -4,13 +4,20 @@ import rospy
 
 from std_msgs.msg import Header, String
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped, PoseArray, Pose, Point, Quaternion
+from geometry_msgs.msg import PoseStamped,
+							  PoseWithCovarianceStamped,
+							  PoseArray,
+							  Pose,
+							  Point,
+							  Quaternion
 from nav_msgs.srv import GetMap
 
 import tf
 from tf import TransformListener
 from tf import TransformBroadcaster
-from tf.transformations import euler_from_quaternion, rotation_matrix, quaternion_from_matrix
+from tf.transformations import euler_from_quaternion, 
+							   rotation_matrix, 
+							   quaternion_from_matrix
 from random import gauss
 
 import math
@@ -21,46 +28,57 @@ from numpy.random import random_sample
 from sklearn.neighbors import NearestNeighbors
 
 class TransformHelpers:
-	""" Some convenience functions for translating between various representions of a robot pose.
+	""" Some convenience functions for translating between various
+		representions of a robot pose.
 		TODO: nothing... you should not have to modify these """
 
 	@staticmethod
 	def convert_translation_rotation_to_pose(translation, rotation):
-		""" Convert from representation of a pose as translation and rotation (Quaternion) tuples to a geometry_msgs/Pose message """
-		return Pose(position=Point(x=translation[0],y=translation[1],z=translation[2]), orientation=Quaternion(x=rotation[0],y=rotation[1],z=rotation[2],w=rotation[3]))
+		""" Convert from representation of a pose as translation and rotation
+			(Quaternion) tuples to a geometry_msgs/Pose message """
+		return Pose(position=Point(x=translation[0], y=translation[1],
+					z=translation[2]),
+					orientation=Quaternion(x=rotation[0], y=rotation[1], 
+					z=rotation[2],w=rotation[3]))
 
 	@staticmethod
 	def convert_pose_inverse_transform(pose):
-		""" Helper method to invert a transform (this is built into the tf C++ classes, but ommitted from Python) """
+		""" Helper method to invert a transform (this is built into
+			the tf C++ classes, but ommitted from Python) """
 		translation = np.zeros((4,1))
 		translation[0] = -pose.position.x
 		translation[1] = -pose.position.y
 		translation[2] = -pose.position.z
 		translation[3] = 1.0
 
-		rotation = (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
+		rotation = (pose.orientation.x, pose.orientation.y,
+					pose.orientation.z, pose.orientation.w)
 		euler_angle = euler_from_quaternion(rotation)
-		rotation = np.transpose(rotation_matrix(euler_angle[2], [0,0,1]))		# the angle is a yaw
+		rotation = np.transpose(rotation_matrix(euler_angle[2], [0,0,1]))
 		transformed_translation = rotation.dot(translation)
 
-		translation = (transformed_translation[0], transformed_translation[1], transformed_translation[2])
+		translation = (transformed_translation[0], transformed_translation[1],
+					   transformed_translation[2])
 		rotation = quaternion_from_matrix(rotation)
 		return (translation, rotation)
 
 	@staticmethod
 	def convert_pose_to_xy_and_theta(pose):
 		""" Convert pose (geometry_msgs.Pose) to a (x,y,yaw) tuple """
-		orientation_tuple = (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
+		orientation_tuple = (pose.orientation.x, pose.orientation.y,
+							 pose.orientation.z, pose.orientation.w)
 		angles = euler_from_quaternion(orientation_tuple)
 		return (pose.position.x, pose.position.y, angles[2])
 
 class Particle:
-	""" Represents a hypothesis (particle) of the robot's pose consisting of x,y and theta (yaw)
+	""" Represents a hypothesis (particle) of the robot's pose consisting
+			of x,y and theta (yaw)
 		Attributes:
 			x: the x-coordinate of the hypothesis relative to the map frame
 			y: the y-coordinate of the hypothesis relative ot the map frame
 			theta: the yaw of the hypothesis relative to the map frame
-			w: the particle weight (the class does not ensure that particle weights are normalized
+			w: the particle weight (the class does not ensure that particle
+				weights are normalized
 	"""
 
 	def __init__(self,x=0.0,y=0.0,theta=0.0,w=1.0):
@@ -68,31 +86,39 @@ class Particle:
 			x: the x-coordinate of the hypothesis relative to the map frame
 			y: the y-coordinate of the hypothesis relative ot the map frame
 			theta: the yaw of the hypothesis relative to the map frame
-			w: the particle weight (the class does not ensure that particle weights are normalized """ 
+			w: the particle weight (the class does not ensure that particle
+				weights are normalized """ 
 		self.w = w
 		self.theta = theta
 		self.x = x
 		self.y = y
 
 	def as_pose(self):
-		""" A helper function to convert a particle to a geometry_msgs/Pose message """
-		orientation_tuple = tf.transformations.quaternion_from_euler(0,0,self.theta)
-		return Pose(position=Point(x=self.x,y=self.y,z=0), orientation=Quaternion(x=orientation_tuple[0], y=orientation_tuple[1], z=orientation_tuple[2], w=orientation_tuple[3]))
+		""" A helper function to convert a particle to a
+			geometry_msgs/Pose message """
+		orientation_tuple = tf.transformations.quaternion_from_euler(0, 0,
+																	 self.theta)
+		return Pose(position=Point(x=self.x, y=self.y, z=0),
+					orientation=Quaternion(x=orientation_tuple[0],
+										   y=orientation_tuple[1],
+										   z=orientation_tuple[2],
+										   w=orientation_tuple[3]))
 
 	# TODO: define additional helper functions if needed
 
 class OccupancyField:
-	""" Stores an occupancy field for an input map.  An occupancy field returns the distance to the closest
-		obstacle for any coordinate in the map
+	""" Stores an occupancy field for an input map.  An occupancy field returns
+		the distance to the closest obstacle for any coordinate in the map
 		Attributes:
 			map: the map to localize against (nav_msgs/OccupancyGrid)
-			closest_occ: the distance for each entry in the OccupancyGrid to the closest obstacle
+			closest_occ: the distance for each entry in the OccupancyGrid to
+						 the closest obstacle
 	"""
 
 	def __init__(self, map):
 		self.map = map		# save this for later
 		# build up a numpy array of the coordinates of each grid cell in the map
-		X = np.zeros((self.map.info.width*self.map.info.height,2))
+		X = np.zeros((self.map.info.width * self.map.info.height,2))
 
 		# while we're at it let's count the number of occupied cells
 		total_occupied = 0
